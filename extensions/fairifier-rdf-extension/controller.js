22c4d6b8eaadf696275a2ef1140299168132aa2a
Shamanou <mandarijnopw8@gmail.com>
2017-04-13 11:01:44 +0200

    Metadata editor update
    The metadata editor whas updated so some changes had to be made

--- extensions/fairifier-rdf-extension/controller.js
+++ extensions/fairifier-rdf-extension/controller.js
@@ -164,11 +164,9 @@ function init() {
             "scripts/externals/rdfstore_min.js",
             "scripts/externals/bootstrap.js",
             "scripts/externals/angular.min.js",
-            "scripts/externals/ui-bootstrap.js",
-            "scripts/externals/ui-bootstrap-tpls-2.5.0.min.js",
-            "/../../../metadata-form/bower_components/angular-bootstrap/ui-bootstrap.min.js",
-            "/../../../metadata-form/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js",
-            "scripts/metadata-form.js",
+            "scripts/externals/ui-bootstrap.min.js",
+            "scripts/externals/ui-bootstrap-tpls.min.js",
+            "scripts/metadata-form.min.js",
             
             
             "scripts/rdf-data-table-view.js",
