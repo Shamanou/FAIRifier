0d050a2aa7487a3d6b2d0bb8b98f323d6714aa47
shamanou <mandarijnopw8@gmail.com>
2017-09-26 11:02:39 +0200

    updated backend

--- extensions/fairifier-rdf-extension/dtls/fairifier/FileSystemRdfSkeletonImpl.java
+++ extensions/fairifier-rdf-extension/dtls/fairifier/FileSystemRdfSkeletonImpl.java
@@ -26,42 +26,30 @@ public class FileSystemRdfSkeletonImpl implements RdfSkeletonService {
     
     @Override
     public List<String> listModels(final String fileType)  throws IOException {
-        List<String[]> files = Files
-                .list(SAVELOCATION)
+        return Files.list(SAVELOCATION)
                 .filter(Files::isRegularFile)
                 .map(this::getStringAndFiletype)
+                .filter(val -> val[0] != null)
+                .filter(val -> val[1].equals(fileType) )
+                .map(element -> element[0])
                 .collect(Collectors.toList());
-        ArrayList<String> outList = new ArrayList<String>();
-        for (String[] o : files) {
-            if (o[0] != null) { 
-                if (o[1].equals(fileType)) {
-                    outList.add(o[0]);
-                }
-            }
-        }
-        return outList;
     }
 
     @Override
     public List<String> listModels()  throws IOException {
-        ArrayList<String> outList = new ArrayList<String>();
-        List<String[]> files = Files
-                .list(SAVELOCATION)
+        return Files.list(SAVELOCATION)
                 .filter(Files::isRegularFile)
                 .map(this::getStringAndFiletype)
+                .filter(val -> val[0] != null)
+                .map(element -> element[0])
                 .collect(Collectors.toList());
-        for (String[] o : files) {
-            if (o != null) { 
-                outList.add(o[0]);
-            }
-        }
-        return outList;
     }
     
     @Override
     public void saveModel(String json, String fileType, String projectId)  throws IOException {
-        Path fileLocation = Paths.get(SAVELOCATION.toString() + File.separator + projectId + ".skeleton.json");
+        Path fileLocation = Paths.get(SAVELOCATION.toString() + File.separator + projectId + ".skeleton.json");        
         Files.write(fileLocation, json.getBytes(StandardCharsets.UTF_8));
+        fileLocation = Paths.get(SAVELOCATION.toString() + File.separator + projectId + ".metadata.skeleton.json");
         try (BufferedWriter writer = Files.newBufferedWriter(fileLocation)) {
             SkeletonMetadata metadata = new SkeletonMetadata();
             metadata.setFileType(fileType);
@@ -73,29 +61,22 @@ public class FileSystemRdfSkeletonImpl implements RdfSkeletonService {
 
     @Override
     public String loadModel(String projectId)  throws IOException {
-        java.util.Optional<String[]> selected = Files.list(SAVELOCATION)
-                .filter(Files::isRegularFile)
-                .filter(path -> {
-                    String filename = path.getFileName().toString();
-                    String[] tokens = filename.split("\\.");
-                    return tokens.length == 3 && tokens[0].equals(projectId);
-                })
-                .map(this::getStringAndFiletype)
-                .findFirst();
-        return selected.get()[1];
+        return new String(Files.readAllBytes(Paths.get(SAVELOCATION.toString() + File.separator + projectId + ".skeleton.json" )));
     }
     
-    public String[] getStringAndFiletype(Path element){
+    private String[] getStringAndFiletype(Path element){
         String name = element.getFileName().toString().split("\\.")[0];
+        String[] out = new String[2];
         try {
             if (new File(element.getParent() + File.separator + element.getFileName().toString()).isFile() && element.getFileName().toString().contains("metadata")) {
                 SkeletonMetadata metadata = mapper.readValue(new File(element.getParent().toString() + File.separator + name + ".metadata.skeleton.json"), SkeletonMetadata.class);
-                String[] out = { name, metadata.getFileType() };
-                return out;
+                
+                out[0] = name;
+                out[1] = metadata.getFileType();
             }
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
-        return null;
+        return out;
     }
 }
