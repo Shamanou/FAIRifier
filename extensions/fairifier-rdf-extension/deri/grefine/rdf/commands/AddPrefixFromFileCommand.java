f9fe8341ca71fb41a382b31dce99cf8b484f574f
shamanou <mandarijnopw8@gmail.com>
2017-10-13 21:10:48 +0200

    Updated prefix loading
    - could not fix reported bug because the parsers of rdf4j are unable to parse the provided file.
    - errors are propagated to front end now
    - more parsers are added extending input compatibility

--- extensions/fairifier-rdf-extension/deri/grefine/rdf/commands/AddPrefixFromFileCommand.java
+++ extensions/fairifier-rdf-extension/deri/grefine/rdf/commands/AddPrefixFromFileCommand.java
@@ -2,23 +2,17 @@ package org.deri.grefine.rdf.commands;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.deri.grefine.rdf.app.ApplicationContext;
 import org.deri.grefine.rdf.vocab.VocabularyImporter;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.eclipse.rdf4j.repository.Repository;;
+import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.rio.RDFFormat;
@@ -26,76 +20,80 @@ import org.eclipse.rdf4j.sail.inferencer.fc.ForwardChainingRDFSInferencer;
 import org.eclipse.rdf4j.sail.memory.MemoryStore;
 import org.json.JSONWriter;
 
-public class AddPrefixFromFileCommand extends RdfCommand{
+public class AddPrefixFromFileCommand extends RdfCommand {
 
     public AddPrefixFromFileCommand(ApplicationContext ctxt) {
-		super(ctxt);
-	}
+        super(ctxt);
+    }
 
-	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-		try {
-			FileItemFactory factory = new DiskFileItemFactory();
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            FileItemFactory factory = new DiskFileItemFactory();
 
-			// Create a new file upload handler
-			ServletFileUpload upload = new ServletFileUpload(factory);
+            // Create a new file upload handler
+            ServletFileUpload upload = new ServletFileUpload(factory);
 
-			String uri = null, prefix = null, format = null, projectId = null, filename="";
-			InputStream in = null;
-			@SuppressWarnings("unchecked")
-			List<FileItem> items = upload.parseRequest(request);
-			for(FileItem item:items){
-				if(item.getFieldName().equals("vocab-prefix")){
-					prefix = item.getString(); 
-				}else if(item.getFieldName().equals("vocab-uri")){
-					uri = item.getString();
-				}else if(item.getFieldName().equals("project")){
-					projectId = item.getString();
-				}else if(item.getFieldName().equals("file_format")){
-					format = item.getString();
-					if(format.equals("text/turtle")){
-						format = "TTL";
-					}else if(format.equals("application/rdf+xml")){
-						format = "RDF/XML";
-					}else if(format.equals("text/rdf+n3")){
-						format = "N3";
-					}else if(format.equals("application/n-triples")){
-						format = "NTRIPLE";
-					}
-				}else{
-					filename = item.getName();
-					in = item.getInputStream();
-				}
-			}
+            String uri = null, prefix = null, format = null, projectId = null, filename = "";
+            InputStream in = null;
+            @SuppressWarnings("unchecked")
+            List<FileItem> items = upload.parseRequest(request);
+            for (FileItem item : items) {
+                if (item.getFieldName().equals("vocab-prefix")) {
+                    prefix = item.getString();
+                } else if (item.getFieldName().equals("vocab-uri")) {
+                    uri = item.getString();
+                } else if (item.getFieldName().equals("project")) {
+                    projectId = item.getString();
+                } else if (item.getFieldName().equals("file_format")) {
+                    format = item.getString();
+                    if (format.equals("text/turtle")) {
+                        format = "TTL";
+                    } else if (format.equals("application/rdf+xml")) {
+                        format = "RDF/XML";
+                    } else if (format.equals("text/rdf+n3")) {
+                        format = "N3";
+                    } else if (format.equals("application/n-triples")) {
+                        format = "NTRIPLE";
+                    }
+                } else {
+                    filename = item.getName();
+                    in = item.getInputStream();
+                }
+            }
 
-			Repository repository = new SailRepository(new ForwardChainingRDFSInferencer(new MemoryStore()));
-			repository.initialize();
-			RepositoryConnection con = repository.getConnection();
-			RDFFormat rdfFormat;
-			if(format.equals("TTL")){
-				rdfFormat = RDFFormat.TURTLE;
-			}else if(format.equals("N3")){
-				rdfFormat = RDFFormat.N3;
-			}else if(format.equals("NTRIPLE")) {
-				rdfFormat = RDFFormat.NTRIPLES;
-			}else{
-				rdfFormat = RDFFormat.RDFXML;
-			}
-			con.add(in, "", rdfFormat);
-			con.close();
+            Repository repository =
+                    new SailRepository(new ForwardChainingRDFSInferencer(new MemoryStore()));
+            repository.initialize();
+            RepositoryConnection con = repository.getConnection();
+            RDFFormat rdfFormat;
+            if (format.equals("TTL")) {
+                rdfFormat = RDFFormat.TURTLE;
+            } else if (format.equals("N3")) {
+                rdfFormat = RDFFormat.N3;
+            } else if (format.equals("NTRIPLE")) {
+                rdfFormat = RDFFormat.NTRIPLES;
+            } else {
+                rdfFormat = RDFFormat.RDFXML;
+            }
+            con.add(in, "", rdfFormat);
+            con.close();
 
-			getRdfSchema(projectId).addPrefix(prefix, uri);
+            getRdfSchema(projectId).addPrefix(prefix, uri);
 
-        	getRdfContext().getVocabularySearcher().importAndIndexVocabulary(prefix, uri, repository, projectId, new VocabularyImporter());
-        	//success
-                JSONWriter writer = new JSONWriter(response.getWriter());
-                writer.object();
-                writer.key("code") ; writer.value("ok");
-                writer.endObject();
+            getRdfContext().getVocabularySearcher().importAndIndexVocabulary(prefix, uri,
+                    repository, projectId, new VocabularyImporter());
+            // success
+            JSONWriter writer = new JSONWriter(response.getWriter());
+            writer.object();
+            writer.key("code");
+            writer.value("ok");
+            writer.endObject();
 
-		} catch (Exception e) {
-		    respondException(response, e);
-		}
+        } catch (Exception e) {
+            respondException(response, e);
+        }
 
-	}
+    }
 }
--- extensions/fairifier-rdf-extension/dtls/fairifier/DetectFileFormatCommand.java
+++ extensions/fairifier-rdf-extension/dtls/fairifier/DetectFileFormatCommand.java
@@ -1,43 +1,38 @@
 package org.dtls.fairifier;
 
-import java.lang.Exception;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import org.deri.grefine.rdf.utils.HttpUtils;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.json.JSONWriter;
-import com.google.refine.commands.Command;
-import org.deri.grefine.rdf.app.ApplicationContext;
-import com.google.refine.browsing.Engine;
-import com.google.refine.model.Project;
-import org.eclipse.rdf4j.rio.RDFFormat;
-import org.eclipse.rdf4j.rio.RDFParser;
-import org.eclipse.rdf4j.rio.nquads.NQuadsParser;
-import org.eclipse.rdf4j.rio.turtle.TurtleParser;
-import org.eclipse.rdf4j.rio.rdfxml.RDFXMLParser;
-import org.eclipse.rdf4j.rio.trig.TriGParser;
-import org.eclipse.rdf4j.rio.ntriples.NTriplesParser;
-import java.lang.System;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.eclipse.rdf4j.rio.Rio;
-import java.io.ByteArrayInputStream;
-import java.nio.charset.StandardCharsets;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import java.io.InputStream;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
-import java.util.List;
 import org.apache.commons.fileupload.FileUploadException;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.eclipse.rdf4j.rio.RDFParser;
+import org.eclipse.rdf4j.rio.jsonld.JSONLDParser;
+import org.eclipse.rdf4j.rio.nquads.NQuadsParser;
+import org.eclipse.rdf4j.rio.ntriples.NTriplesParser;
+import org.eclipse.rdf4j.rio.rdfjson.RDFJSONParser;
+import org.eclipse.rdf4j.rio.rdfxml.RDFXMLParser;
+import org.eclipse.rdf4j.rio.trig.TriGParser;
+import org.eclipse.rdf4j.rio.trix.TriXParser;
+import org.eclipse.rdf4j.rio.turtle.TurtleParser;
+import org.json.JSONWriter;
+import com.google.refine.commands.Command;
 
 /**
  * Enables automatic file format detection
+ * 
  * @author Shamanou van Leeuwen
  * @date May 24 2017
  *
@@ -45,36 +40,44 @@ import java.io.BufferedReader;
 
 
 /**
- * A command which detects the file format, should be configured in the controller 
- * and called from the javascript front-end.
+ * A command which detects the file format, should be configured in the controller and called from
+ * the javascript front-end.
  * 
  */
-public class DetectFileFormatCommand extends Command{
+public class DetectFileFormatCommand extends Command {
     RDFParser rdfParserttl = new TurtleParser();
     RDFParser rdfParserntriples = new NTriplesParser();
+    RDFParser rdfParserrdfxml = new RDFXMLParser();
+    RDFParser rdfParserJsonld = new JSONLDParser();
+    RDFParser rdfParserNQuads = new NQuadsParser();
+    RDFParser rdfParserJson = new RDFJSONParser();
+    RDFParser rdfParserTrix = new TriXParser();
+    RDFParser rdfParserTrig = new TriGParser();
 
     RDFParser[] parsers;
 
     /**
-     * This method takes a request containing rdf and tries to iterate over 
-     * different parsers to see which one is able to parse the data.
-     * If it is able to be parsed a format string is returned by passing it 
-     * to the response object in JSON-format. 
+     * This method takes a request containing rdf and tries to iterate over different parsers to see
+     * which one is able to parse the data. If it is able to be parsed a format string is returned
+     * by passing it to the response object in JSON-format.
      * 
      * When there is no format detected the parser defaults to rdf/xml.
      * 
-     * Other formats that can be parsed are Turtle and Ntripples.
-     * NOTE: Sesame supports more formats but the currently used version 
-     * gave problems with other parsers. 
+     * Other formats that can be parsed are Turtle and Ntripples. NOTE: Sesame supports more formats
+     * but the currently used version gave problems with other parsers.
      * 
      * @param req a request object
      * @param res a response object
-     * @see com.google.refine.commands.Command#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     * @see com.google.refine.commands.Command#doPost(javax.servlet.http.HttpServletRequest,
+     *      javax.servlet.http.HttpServletResponse)
      */
     @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        
-        this.parsers = new RDFParser[]{this.rdfParserntriples, this.rdfParserttl};
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        this.parsers = new RDFParser[] {this.rdfParserntriples, this.rdfParserttl,
+                this.rdfParserrdfxml, this.rdfParserJsonld, this.rdfParserNQuads,
+                this.rdfParserJson, this.rdfParserTrix, this.rdfParserTrig};
         FileItemFactory factory = new DiskFileItemFactory();
 
         // Create a new file upload handler
@@ -82,64 +85,69 @@ public class DetectFileFormatCommand extends Command{
         List<FileItem> items = null;
         String baseuri = null;
         String filename = null;
-        InputStream in = null;
+        InputStream[] in = new InputStream[this.parsers.length];
         String format = null;
         String url = null;
-        
-        try{
+
+        try {
             items = upload.parseRequest(req);
-            for(FileItem item:items){
-                if(item.getFieldName().equals("baseuri")){
+            for (FileItem item : items) {
+                if (item.getFieldName().equals("baseuri")) {
                     baseuri = item.getString();
-                }else if (item.getFieldName().equals("file_source")) {
+                } else if (item.getFieldName().equals("file_source")) {
                     url = item.getString();
-                }else if(item.getFieldName().equals("file_upload")){
+                } else if (item.getFieldName().equals("file_upload")) {
                     filename = item.getName();
-                    in = item.getInputStream();
+                    for (int i = 0; i < in.length; i++) {
+                        StringWriter writer = new StringWriter();
+                        in[i] = new BufferedInputStream(item.getInputStream());
+
+                    }
                 }
             }
-        }catch(FileUploadException ex){
+        } catch (FileUploadException ex) {
             respondException(res, ex);
         }
+        StringBuffer response = new StringBuffer();;
 
         if ((url != null) && !url.trim().equals("") && !url.trim().equals("url")) {
             URL obj = new URL(url);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             BufferedReader inr = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
-            StringBuffer response = new StringBuffer();
 
             while ((inputLine = inr.readLine()) != null) {
-                    response.append(inputLine);
+                response.append(inputLine);
             }
             inr.close();
-            in = new ByteArrayInputStream(response.toString().getBytes());
+            for (int i = 0; i < in.length; i++) {
+                in[i] = new BufferedInputStream(
+                        new ByteArrayInputStream(response.toString().getBytes("UTF8")));
+            }
         }
-        for (RDFParser parser: parsers){
-            try{
-                if( baseuri == null) {
+        for (int i = 0; i < parsers.length; i++) {
+            try {
+                if (baseuri == null) {
                     baseuri = "";
                 }
-                parser.parse(in, baseuri);
-                format = parser.getRDFFormat().getDefaultMIMEType();
+                parsers[i].parse(in[i], baseuri);
+                format = parsers[i].getRDFFormat().getDefaultMIMEType();
                 break;
-            }catch(Exception e){
-                System.out.println(e.toString());    
+            } catch (Exception e) {
+                System.out.println(
+                        parsers[i].getRDFFormat().getDefaultMIMEType() + " : " + e.toString());
                 continue;
             }
         }
-        if (format == null) {
-            format = "application/rdf+xml";
-        }
-        
-        try{
+        try {
             res.setCharacterEncoding("UTF-8");
             res.setHeader("Content-Type", "application/json");
             JSONWriter writer = new JSONWriter(res.getWriter());
             writer.object();
-            writer.key("RDFFormat"); writer.value(format);
+            writer.key("RDFFormat");
+            writer.value(format);
             writer.endObject();
-        }catch(Exception e){
+        } catch (Exception e) {
             respondException(res, e);
         }
     }
