ba7c909cce487303224d613f23234b45642e4c6a
shamanou <mandarijnopw8@gmail.com>
2017-10-23 23:46:53 +0200

    Improved code style and added conformations before overwriting rdfskeleton
    - Improved code style of loadrdfskeletoncommand
    - Improved code style in touched javascript
    - added functionality to elmts._load_skeleton.click(function(e)

--- extensions/fairifier-rdf-extension/dtls/fairifier/LoadRdfSkeletonCommand.java
+++ extensions/fairifier-rdf-extension/dtls/fairifier/LoadRdfSkeletonCommand.java
@@ -4,16 +4,13 @@ package org.dtls.fairifier;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Properties;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.deri.grefine.rdf.RdfSchema;
 import org.deri.grefine.rdf.operations.LoadRdfSchemaOperation;
 import org.json.JSONObject;
 import org.json.JSONWriter;
-
 import com.google.refine.commands.Command;
 import com.google.refine.history.HistoryEntry;
 import com.google.refine.model.AbstractOperation;
@@ -32,7 +29,8 @@ public class LoadRdfSkeletonCommand extends Command {
         res.setHeader("Content-Type", "application/json");
         JSONWriter writer = new JSONWriter(res.getWriter());
         try {
-            Project project = getProject(req); // this needs the project parameter, indicating the project on which the
+            Project project = getProject(req); // this needs the project parameter, indicating the
+                                               // project on which the
                                                // skeleton should be applied
 
             String jsonString = req.getParameter("schema");
@@ -49,11 +47,13 @@ public class LoadRdfSkeletonCommand extends Command {
         }
     }
 
-    private void performProcess(HttpServletRequest request, HttpServletResponse response, Project project,
-            Process process)
-            throws Exception {
-        Model out = rdfSkeletonService.loadModel(request.getParameter("projectId")); // this is the project where the
-                                                                                     // skeleton comes from
+    private void performProcess(HttpServletRequest request, HttpServletResponse response,
+            Project project, Process process) throws Exception {
+        Model out = rdfSkeletonService.loadModel(request.getParameter("projectId")); // this is the
+                                                                                     // project
+                                                                                     // where the
+                                                                                     // skeleton
+                                                                                     // comes from
         response.setCharacterEncoding("UTF-8");
         response.setHeader("Content-Type", "application/json");
 
--- extensions/fairifier-rdf-extension/scripts/rdf-skeleton-list.js
+++ extensions/fairifier-rdf-extension/scripts/rdf-skeleton-list.js
@@ -1,100 +1,105 @@
-var listData = {};
-
-function RdfSkeletonListDialog(func, schema, project, baseuri){
-    this._createDialog();
-    this.func = func;
-    this._schema = schema;
-    this._project = project;
-    this._rdfSkeleton = null;
-    this._baseuri = baseuri;
+function RdfSkeletonListDialog(func, schema, project, baseuri) {
+  this._createDialog();
+  this.func = func;
+  this._schema = schema;
+  this._project = project;
+  this._rdfSkeleton = null;
+  this._baseuri = baseuri;
 };
 
 RdfSkeletonListDialog.prototype._createDialog = function() {
-    var self = this;
-    var frame = DialogSystem.createDialog();
-    
-    frame.width("800px");
-    
-    var header = $('<div></div>').addClass("dialog-header").text("RDF Skeleton List").appendTo(frame);
-    var body = $('<div></div>').css('height', '250px').addClass("dialog-body").appendTo(frame);
-    var footer = $('<div></div>').addClass("dialog-footer").appendTo(frame);
-    
-    this._constructFooter(footer);
-    this._constructBody(body);
-    this._level = DialogSystem.showDialog(frame);
+  var self = this;
+  var frame = DialogSystem.createDialog();
+
+  frame.width("800px");
+
+  var header = $('<div></div>').addClass("dialog-header").text(
+          "RDF Skeleton List").appendTo(frame);
+  var body = $('<div></div>').css('height', '250px').addClass("dialog-body")
+          .appendTo(frame);
+  var footer = $('<div></div>').addClass("dialog-footer").appendTo(frame);
+
+  this._constructFooter(footer);
+  this._constructBody(body);
+  this._level = DialogSystem.showDialog(frame);
 };
 
 RdfSkeletonListDialog.prototype._constructFooter = function(footer) {
-    var self = this;
-    
-    $('<button></button>').addClass('button').html("&nbsp;&nbsp;Load&nbsp;&nbsp;").click(function() {
-        self._schema['baseUri'] = self._baseuri;
-        self._schema = JSON.stringify(self._schema);
-        $.post("command/rdf-extension/load-rdf-skeleton",  {
-                project: self._project,
-                schema : self._schema,
-                projectId : listData[$( "select: selected" ).attr('id')].project
-            },function(data){
-                self.func(data);
-        });
-    }).appendTo(footer);
-    
-    $('<button></button>').addClass('button').text("Cancel").click(function() {
-        DialogSystem.dismissUntil(self._level - 1);
-    }).appendTo(footer);
+  var self = this;
+
+  $('<button></button>').addClass('button')
+          .html("&nbsp;&nbsp;Load&nbsp;&nbsp;").click(
+                  function() {
+                    self._schema['baseUri'] = self._baseuri;
+                    self._schema = JSON.stringify(self._schema);
+                    $.post("command/rdf-extension/load-rdf-skeleton", {
+                      project: self._project,
+                      schema: self._schema,
+                      projectId: self.listData[$("select").find(":selected")
+                              .attr('id')].project
+                    }, function(data) {
+                      self.func(data);
+                    });
+                  }).appendTo(footer);
+
+  $('<button></button>').addClass('button').text("Cancel").click(function() {
+    DialogSystem.dismissUntil(self._level - 1);
+  }).appendTo(footer);
 };
 
 RdfSkeletonListDialog.prototype._constructBody = function(body) {
-    var self = this;
-    $('<p>' +
-            'Select a RDF skeleton saved earlier to apply to your data.' +
-        '</p>').appendTo(body);
-    
-   var container = $('<div id="body" style="display:block;">');
-    var html = $('<div style="float:left;">');
-    $.post("command/rdf-extension/list-rdf-skeletons",function(data){
-       var list = $('<select multiple width="300" style="width: 300px;">');
-        var data = data.list
-        listData = data;
-        for (var i = 0; i < data.length; i++){
-           var element = data[i];
-           var option = $('<option id='+ i + '>' + element.name + '</option>');
-           
-           option.click(function(evt){
-               evt.preventDefault();
-               $(".preview").html('');
-               var skeleton = JSON.parse(data[event.target.id].skeleton);
-               $("<h2>prefixes</h2>").appendTo(".preview")
-               var prefList = $("<ul>");
-               for (var x = 0; x < skeleton.prefixes.length; x++){
-                   $("<li>").html(skeleton.prefixes[x].name + ' - ')
-                   .append($("<a>").html(skeleton.prefixes[x].uri))
-                   .attr("href", skeleton.prefixes[x].uri)
-                   .appendTo(prefList);
-               }
-               $("</ul>").appendTo(prefList);
-               prefList.appendTo(".preview");
-               $("<h2>properties</h2>").appendTo(".preview")
-               var propList = $("<ul>");
-               for (var z = 0; z < skeleton.rootNodes.length; z++){
-                   for (var y = 0 ; y < skeleton.rootNodes[z].links.length; y++){
-                       $("<li>").html(skeleton.rootNodes[z].links[y].target.columnName + ' - ')
-                           .append($("<a>").html(skeleton.rootNodes[z].links[y].uri))
-                           .attr("href",  skeleton.rootNodes[z].links[y].uri)
-                           .appendTo(propList);
-                   }
-               }
-               $("</ul>").appendTo(propList);
-               propList.appendTo(".preview");
-           });
-           option.appendTo(list);
+  var self = this;
+  $(
+          '<p>' + 'Select a RDF skeleton saved earlier to apply to your data.'
+                  + '</p>').appendTo(body);
+
+  var container = $('<div id="body" style="display:block;">');
+  var html = $('<div style="float:left;">');
+  $.post("command/rdf-extension/list-rdf-skeletons", function(data) {
+    var list = $('<select multiple width="300" style="width: 300px;">');
+    var data = data.list
+    self.listData = data;
+    for (var i = 0; i < data.length; i++) {
+      var element = data[i];
+      var option = $('<option id=' + i + '>' + element.name + '</option>');
+
+      option.click(function(evt) {
+        evt.preventDefault();
+        $(".preview").html('');
+        var skeleton = JSON.parse(data[event.target.id].skeleton);
+        $("<h2>prefixes</h2>").appendTo(".preview")
+        var prefList = $("<ul>");
+        for (var x = 0; x < skeleton.prefixes.length; x++) {
+          $("<li>").html(skeleton.prefixes[x].name + ' - ').append(
+                  $("<a>").html(skeleton.prefixes[x].uri)).attr("href",
+                  skeleton.prefixes[x].uri).appendTo(prefList);
         }
-        $('</select>').appendTo(list);
-        list.appendTo(html);
-    });
-    $('</div>').appendTo(html);
-    $(html).appendTo(container);
-    $('<div class="preview" style="overflow:auto;height:75%;float:right;"></div></div>').appendTo(container);
-    $(container).appendTo(body);
+        $("</ul>").appendTo(prefList);
+        prefList.appendTo(".preview");
+        $("<h2>properties</h2>").appendTo(".preview")
+        var propList = $("<ul>");
+        for (var z = 0; z < skeleton.rootNodes.length; z++) {
+          for (var y = 0; y < skeleton.rootNodes[z].links.length; y++) {
+            $("<li>").html(
+                    skeleton.rootNodes[z].links[y].target.columnName + ' - ')
+                    .append($("<a>").html(skeleton.rootNodes[z].links[y].uri))
+                    .attr("href", skeleton.rootNodes[z].links[y].uri).appendTo(
+                            propList);
+          }
+        }
+        $("</ul>").appendTo(propList);
+        propList.appendTo(".preview");
+      });
+      option.appendTo(list);
+    }
+    $('</select>').appendTo(list);
+    list.appendTo(html);
+  });
+  $('</div>').appendTo(html);
+  $(html).appendTo(container);
+  $(
+          '<div class="preview" style="overflow:auto;height:75%;float:right;"></div></div>')
+          .appendTo(container);
+  $(container).appendTo(body);
 
 };
