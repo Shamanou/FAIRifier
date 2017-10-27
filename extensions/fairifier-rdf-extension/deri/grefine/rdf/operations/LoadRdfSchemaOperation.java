c4d5ccdda974f69364a80c5f3628b620315a039a
shamanou <mandarijnopw8@gmail.com>
2017-10-27 23:27:46 +0200

    Improved code style of loadrdfschemoperation

--- extensions/fairifier-rdf-extension/deri/grefine/rdf/operations/LoadRdfSchemaOperation.java
+++ extensions/fairifier-rdf-extension/deri/grefine/rdf/operations/LoadRdfSchemaOperation.java
@@ -1,22 +1,15 @@
 package org.deri.grefine.rdf.operations;
 
-import java.io.IOException;
-import java.io.LineNumberReader;
-import java.io.Writer;
 import java.util.Properties;
-
 import org.deri.grefine.rdf.RdfSchema;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONWriter;
-
 import com.google.refine.history.Change;
 import com.google.refine.history.HistoryEntry;
 import com.google.refine.model.AbstractOperation;
 import com.google.refine.model.Project;
 import com.google.refine.operations.OperationRegistry;
-import com.google.refine.util.ParsingUtilities;
-import com.google.refine.util.Pool;
 
 public class LoadRdfSchemaOperation extends AbstractOperation {
 
@@ -26,14 +19,12 @@ public class LoadRdfSchemaOperation extends AbstractOperation {
         this._schema = schema;
     }
 
-    static public AbstractOperation reconstruct(Project project, JSONObject obj)
-            throws Exception {
-        return new LoadRdfSchemaOperation(RdfSchema.reconstruct(obj
-                .getJSONObject("schema")));
+    static public AbstractOperation reconstruct(Project project, JSONObject obj) throws Exception {
+        return new LoadRdfSchemaOperation(RdfSchema.reconstruct(obj.getJSONObject("schema")));
     }
 
-    public void write(JSONWriter writer, Properties options)
-            throws JSONException {
+    @Override
+    public void write(JSONWriter writer, Properties options) throws JSONException {
         writer.object();
         writer.key("op");
         writer.value(OperationRegistry.s_opClassToName.get(this.getClass()));
@@ -51,11 +42,13 @@ public class LoadRdfSchemaOperation extends AbstractOperation {
     }
 
     @Override
-    protected HistoryEntry createHistoryEntry(Project project,
-            long historyEntryID) throws Exception {
+    protected HistoryEntry createHistoryEntry(Project project, long historyEntryID)
+            throws Exception {
         String description = "Load RDF schema skeleton";
-        
-        return new HistoryEntry(historyEntryID, project, description,
-                LoadRdfSchemaOperation.this, new SaveRdfSchemaOperation.RdfSchemaChange(_schema));
+
+        Change change = new SaveRdfSchemaOperation.RdfSchemaChange(_schema);
+
+        return new HistoryEntry(historyEntryID, project, description, LoadRdfSchemaOperation.this,
+                change);
     }
 }
