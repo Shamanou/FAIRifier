13765a2cafe991eaa2e7d959e01233ef61d4eac8
shamanou <mandarijnopw8@gmail.com>
2017-11-22 07:08:35 +0100

    added check for undefined or empty prefix if this is the case the new prefixes list is empty

--- module/scripts/rdf-schema-alignment.js
+++ module/scripts/rdf-schema-alignment.js
@@ -1,49 +1,49 @@
 var RdfSchemaAlignment = {};
 
 function RdfSchemaAlignmentDialog(schema) {
-  
   this._originalSchema = schema || {
     rootNodes: []
   };
   // this is what can be munched on
   this._schema = cloneDeep(this._originalSchema);
-  
+
   if (!this._schema.rootNodes.length) {
     this._schema.rootNodes.push(RdfSchemaAlignment.createNewRootNode());
   }
-  
+
   this._nodeUIs = [];
   this._createDialog();
-  
+
   RdfSchemaAlignment._defaultNamespace = this._schema.baseUri;
-  
+
   // initialize vocabularyManager
   this._prefixesManager = new RdfPrefixesManager(this, this._schema.prefixes);
   var url = window.location.protocol + "//" + window.location.host + "/";
   this._replaceBaseUri(RdfSchemaAlignment._defaultNamespace || url, true);
-};
+}
+;
 
 RdfSchemaAlignmentDialog.prototype._createDialog = function() {
   var self = this;
   var frame = DialogSystem.createDialog();
-  
+
   frame.width("1000px");
-  
+
   var header = $('<div></div>').addClass("dialog-header").text("RDF Schema Alignment").appendTo(frame);
   var body = $('<div></div>').addClass("dialog-body").appendTo(frame);
   var footer = $('<div></div>').addClass("dialog-footer").appendTo(frame);
-  
+
   this._constructFooter(footer);
   this._constructBody(body);
-  
+
   this._level = DialogSystem.showDialog(frame);
-  
+
   this._renderBody(body);
 };
 
 RdfSchemaAlignmentDialog.prototype._constructFooter = function(footer) {
   var self = this;
-  
+
   $('<button></button>').addClass('button').html("&nbsp;&nbsp;OK&nbsp;&nbsp;").click(function() {
     var schema = self.getJSON();
     Refine.postProcess("rdf-extension", "save-rdf-schema", {}, {
@@ -55,7 +55,7 @@ RdfSchemaAlignmentDialog.prototype._constructFooter = function(footer) {
       }
     });
   }).appendTo(footer);
-  
+
   $('<button></button>').addClass('button').text("Cancel").click(function() {
     DialogSystem.dismissUntil(self._level - 1);
   }).appendTo(footer);
@@ -64,18 +64,18 @@ RdfSchemaAlignmentDialog.prototype._constructFooter = function(footer) {
 RdfSchemaAlignmentDialog.prototype._constructBody = function(body) {
   var self = this;
   $('<p>The Semantic model below specifies how the RDF data that will get generated from your grid-shaped data. ' + 'The cells in each record of your data will get placed into nodes within the model. Configure the model by specifying which column to substitute into which node.' + '</p>').appendTo(body);
-  
+
   var html = $('<p class="base-uri-space"><span class="emphasized">Base URI:</span> <span bind="baseUriSpan" ></span> <a href="#" bind="editBaseUriLink">edit</a></p>' + '<div id="rdf-schema-alignment-tabs" class="refine-tabs">' + '<ul>' + '<li><a href="#rdf-schema-alignment-tabs-schema">Semantic model</a></li>' + '<li><a href="#rdf-schema-alignment-tabs-preview">RDF Preview</a></li>' + '</ul>' + '<div id="rdf-schema-alignment-tabs-schema">' + '<div class="rdf-scheme-dialog-subheader"><table><tr><td><span style="display:block;width:150px;">Available Prefixes:</span></td><td><div class="rdf-schema-prefixes" bind="rdf_schema_prefixes"></div></td></tr></table></div>' + '<div class="schema-alignment-dialog-canvas rdf-schema-alignment-dialog-canvas"></div>' + '<div class="rdf-schema-alignment-body-footer"><a bind="add_another_root_node" href="#">Add another root node</a><a bind="_save_skeleton" href="#" style="float:right">&nbsp;&nbsp;&nbsp;&nbsp;Save&nbsp;&nbsp;&nbsp;&nbsp;</a>' + '<a bind="_share_skeleton" href="#" style="float:right">&nbsp;&nbsp;&nbsp;&nbsp;Share&nbsp;&nbsp;&nbsp;&nbsp;</a><a bind="_load_skeleton" href="#" style="float:right">&nbsp;&nbsp;&nbsp;&nbsp;Load&nbsp;&nbsp;&nbsp;&nbsp;</a>' + '</div>' + '</div>' + '<div id="rdf-schema-alignment-tabs-preview" style="display: none;">' + '<div class="rdf-scheme-dialog-subheader">This is a sample <code>Turtle</code> representation of (up-to) the <em>first 10</em> rows</div>' + '<div class="rdf-schema-alignment-dialog-preview" id="rdf-schema-alignment-dialog-preview"></div>' + '</div>' + '</div>').appendTo(body);
   var elmts = DOM.bind(html);
   this._baseUriSpan = elmts.baseUriSpan;
   this._rdf_schema_prefixes = elmts.rdf_schema_prefixes;
   elmts.baseUriSpan.text(RdfSchemaAlignment._defaultNamespace);
-  
+
   elmts.editBaseUriLink.click(function(evt) {
     evt.preventDefault();
     self._editBaseUri($(evt.target));
   });
-  
+
   elmts._load_skeleton.click(function(e) {
     e.preventDefault();
     rdfSkeletonListDialog = new RdfSkeletonListDialog(function(rdfSkeleton) {
@@ -96,52 +96,59 @@ RdfSchemaAlignmentDialog.prototype._constructBody = function(body) {
       }
     }, function(rdfSkeleton) {
       $.getJSON("command/core/get-operations?" + $.param({
-        project: theProject.id
-      }), function(json) {
-        var operations_old = json.entries.map(function(val) {
-          return val['operation'];
-        });
-        
-        if (operations_old.length == 1) {
-          operations_old = [];
-        } else {
-          operations_old = operations_old.slice(-5);
-        }
-        var operations_new = JSON.parse(rdfSkeleton['skeleton']).operations;
-        Refine.postCoreProcess("apply-operations", {}, {
-          operations: JSON.stringify(operations_old.concat(operations_new))
-        }, {
-          everythingChanged: true
-        }, {
-          onDone: function(o) {
-            if (o.code == "pending") {
-              Refine.update({
-                everythingChanged: true
-              });
-            }
+          project: theProject.id
+        }), function(json) {
+          var operations_old = json.entries.map(function(val) {
+            return val['operation'];
+          });
+
+          if (operations_old.length == 1) {
+            operations_old = [];
+          } else {
+            operations_old = operations_old.slice(-5);
           }
+          var operations_new = JSON.parse(rdfSkeleton['skeleton']).operations;
+          Refine.postCoreProcess("apply-operations", {}, {
+            operations: JSON.stringify(operations_old.concat(operations_new))
+          }, {
+            everythingChanged: true
+          }, {
+            onDone: function(o) {
+              if (o.code == "pending") {
+                Refine.update({
+                  everythingChanged: true
+                });
+              }
+            }
+          });
         });
-      });
       var prefixes = [];
       var new_prefixes = JSON.parse(rdfSkeleton['skeleton']).prefixes;
+      if ((new_prefixes != undefined) && (new_prefixes instanceof Array)) {
+        new_prefixes = JSON.parse(rdfSkeleton['skeleton']).prefixes;
+      } else {
+        new_prefixes = [];
+      }
+
       if (self._schema.prefixes != undefined) {
         prefixes = prefixes.concat(self._schema.prefixes).concat(new_prefixes);
+      } else if (self._schema.prefixes.length == 0) {
+        prefixes = self._schema.prefixes;
       } else {
         prefixes = prefixes.concat(new_prefixes);
       }
-      
+
       var prefixes_unique = [];
       var lookup = {};
       for (var item, i = 0; item = prefixes[i++];) {
         var name = item.uri;
-        
+
         if (!(name in lookup)) {
           lookup[name] = 1;
           prefixes_unique.push(item);
         }
       }
-      console.log(prefixes_unique);
-      
+
       self._originalSchema = theProject.overlayModels.rdfSchema;
       self._schema = cloneDeep(self._originalSchema);
       self._nodeUIs = [];
@@ -150,11 +157,11 @@ RdfSchemaAlignmentDialog.prototype._constructBody = function(body) {
       DialogSystem.dismissUntil(self._level);
     }, self._schema, theProject.id, RdfSchemaAlignment._defaultNamespace);
   });
-  
+
   elmts._save_skeleton.click(function(e) {
     e.preventDefault();
     var schema = self.getJSON();
-    
+
     Refine.postProcess("rdf-extension", "save-rdf-schema", {}, {
       schema: JSON.stringify(schema)
     }, {}, {
@@ -163,7 +170,7 @@ RdfSchemaAlignmentDialog.prototype._constructBody = function(body) {
       }
     });
   });
-  
+
   elmts._share_skeleton.click(function(e) {
     e.preventDefault();
     var schema = self.getJSON();
@@ -171,24 +178,24 @@ RdfSchemaAlignmentDialog.prototype._constructBody = function(body) {
       var schema = self.getJSON();
       var title = $("input#title").val();
       var fileType = $("input#fileType").val();
-      
+
       var operations = $.getJSON("command/core/get-operations?" + $.param({
-        project: theProject.id
-      }), function(json) {
-        var model = self.getJSON();
-        model['operations'] = json.entries.map(function(val) {
-          return val['operation'];
+          project: theProject.id
+        }), function(json) {
+          var model = self.getJSON();
+          model['operations'] = json.entries.map(function(val) {
+            return val['operation'];
+          });
+          $.post("command/rdf-extension/save-rdf-skeleton", {
+            model: JSON.stringify(model),
+            projectId: theProject.id,
+            title: title
+          }, function(data) {
+            console.log(data);
+            alert('RDF skeleton saved sucessfully');
+          });
         });
-        $.post("command/rdf-extension/save-rdf-skeleton", {
-          model: JSON.stringify(model),
-          projectId: theProject.id,
-          title: title
-        }, function(data) {
-          console.log(data);
-          alert('RDF skeleton saved sucessfully');
-        });
-      });
-      
+
       Refine.postProcess("rdf-extension", "save-rdf-schema", {}, {
         schema: JSON.stringify(schema)
       }, {}, {
@@ -198,7 +205,7 @@ RdfSchemaAlignmentDialog.prototype._constructBody = function(body) {
       });
     });
   });
-  
+
   elmts.add_another_root_node.click(function(e) {
     e.preventDefault();
     var newRootNode = RdfSchemaAlignment.createNewRootNode(false)
@@ -207,7 +214,7 @@ RdfSchemaAlignmentDialog.prototype._constructBody = function(body) {
       expanded: true
     }));
   });
-  
+
 };
 
 RdfSchemaAlignmentDialog.prototype._previewRdf = function() {
@@ -215,19 +222,19 @@ RdfSchemaAlignmentDialog.prototype._previewRdf = function() {
   var schema = this.getJSON();
   self._previewPane.empty().html('<img src="images/large-spinner.gif" title="loading..."/>');
   $.post("command/rdf-extension/preview-rdf?" + $.param({
-    project: theProject.id
-  }), {
-    schema: JSON.stringify(schema),
-    engine: JSON.stringify(ui.browsingEngine.getJSON())
-  }, function(data) {
-    self._previewPane.empty();
-    self._previewPane.html(linkify('<pre>' + data.v + '</pre>'));
-  }, "json");
+      project: theProject.id
+    }), {
+      schema: JSON.stringify(schema),
+      engine: JSON.stringify(ui.browsingEngine.getJSON())
+    }, function(data) {
+      self._previewPane.empty();
+      self._previewPane.html(linkify('<pre>' + data.v + '</pre>'));
+    }, "json");
 };
 
 RdfSchemaAlignmentDialog.prototype._renderBody = function(body) {
   var self = this;
-  
+
   $("#rdf-schema-alignment-tabs").tabs({
     activate: function(evt, tabs) {
       if (tabs.newTab.index() === 1) {
@@ -244,17 +251,17 @@ RdfSchemaAlignmentDialog.prototype._renderBody = function(body) {
   });
   //
   // $("#rdf-schema-alignment-tabs-vocabulary-manager").css("display", "");
-  
+
   this._canvas = $(".schema-alignment-dialog-canvas");
   $("table.schema-alignment-table-layout").remove();
   this._nodeTable = $('<table>').addClass("schema-alignment-table-layout").addClass("rdf-schema-alignment-table-layout").appendTo(this._canvas)[0];
-  
+
   for (var i = 0; i < this._schema.rootNodes.length; i++) {
     this._nodeUIs.push(new RdfSchemaAlignmentDialog.UINode(this, this._schema.rootNodes[i], this._nodeTable, {
       expanded: true
     }));
   }
-  
+
   this._previewPane = $("#rdf-schema-alignment-dialog-preview");
 };
 
@@ -283,7 +290,7 @@ RdfSchemaAlignment.createNewRootNode = function(withDefaultChildren) {
     });
   }
   rootNode.links = links;
-  
+
   return rootNode;
 };
 
@@ -291,8 +298,7 @@ RdfSchemaAlignmentDialog.prototype._editBaseUri = function(src) {
   var self = this;
   var menu = MenuSystem.createMenu().width('400px');
   menu.html('<div class="schema-alignment-link-menu-type-search"><input type="text" bind="newBaseUri" size="50"><br/>' + '<button class="button" bind="applyButton">Apply</button>' + '<button class="button" bind="cancelButton">Cancel</button></div>');
-  MenuSystem.showMenu(menu, function() {
-  });
+  MenuSystem.showMenu(menu, function() {});
   MenuSystem.positionMenuLeftRight(menu, src);
   var elmts = DOM.bind(menu);
   elmts.newBaseUri.val(RdfSchemaAlignment._defaultNamespace).focus().select();
@@ -301,7 +307,7 @@ RdfSchemaAlignmentDialog.prototype._editBaseUri = function(src) {
     MenuSystem.dismissAll();
     self._replaceBaseUri(newBaseUri);
   });
-  
+
   elmts.cancelButton.click(function() {
     MenuSystem.dismissAll();
   });
@@ -315,16 +321,16 @@ RdfSchemaAlignmentDialog.prototype._replaceBaseUri = function(newBaseUri, doNotS
   }
   if (!doNotSave) {
     $.post("command/rdf-extension/save-baseURI?" + $.param({
-      project: theProject.id
-    }), {
-      baseURI: newBaseUri
-    }, function(data) {
-      if (data.code === "error") {
-        alert('Error:' + data.message)
-      } else {
-        self._baseUriSpan.empty().text(newBaseUri);
-      }
-    }, "json");
+        project: theProject.id
+      }), {
+        baseURI: newBaseUri
+      }, function(data) {
+        if (data.code === "error") {
+          alert('Error:' + data.message)
+        } else {
+          self._baseUriSpan.empty().text(newBaseUri);
+        }
+      }, "json");
   } else {
     self._baseUriSpan.empty().text(newBaseUri);
   }
@@ -338,7 +344,7 @@ RdfSchemaAlignmentDialog.prototype.getJSON = function() {
       rootNodes.push(node);
     }
   }
-  
+
   var prefixes = [];
   for (var i = 0; i < this._prefixesManager._prefixes.length; i++) {
     prefixes.push({
@@ -357,7 +363,9 @@ RdfSchemaAlignmentDialog._findColumn = function(columnName) {
   var columns = theProject.columnModel.columns;
   for (var i = 0; i < columns.length; i++) {
     var column = columns[i];
-    if (column.name == columnName) { return column; }
+    if (column.name == columnName) {
+      return column;
+    }
   }
   return null;
 };
