e2c73a8c65d14e6b2fc3c97d1ae9c475dfd8f506
shamanou <mandarijnopw8@gmail.com>
2017-11-29 16:28:39 +0100

    improved pr

--- extensions/fairifier-rdf-extension/dtls/fairifier/VirtuosoResource.java
+++ extensions/fairifier-rdf-extension/dtls/fairifier/VirtuosoResource.java
@@ -55,7 +55,7 @@ public class VirtuosoResource extends Resource {
             this.out = this.getModelString();
             HttpResponse res = HttpUtils.put(this.host + this.directory + this.filename, this.out,
                     this.username, this.password);
-            if (res.getStatusLine().getStatusCode() > 400) {
+            if (res.getStatusLine().getStatusCode() >= 400) {
                 throw new HttpException("Pushing data to Virtuso failed");
             }
         }
